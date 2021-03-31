eaed72e1522a8a8c152b269c1e7919abf7cb713a
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-03-30 18:52:52 -0600

    Added Maze Generation and basic character of Science Level

---
+++ Assets/Scripts/MoveCharMaze.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharMaze : MonoBehaviour {
+	public float speedX = 5;
+	public float speedY = 5;
+	private Rigidbody2D rigidBody;
+	
+	void Start() {
+        	rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		// Horizontal move detection
+        	float moveHorizontal = Input.GetAxis("Horizontal");
+	        rigidBody.velocity = new Vector2(moveHorizontal*speedX, rigidBody.velocity.y);
+        	// Vertical move detection
+	        float moveVertically = Input.GetAxis("Vertical");
+       		rigidBody.velocity = new Vector2(rigidBody.velocity.x, moveVertically*speedY);
+    	}
+}
+
