7ddf39b6934279418fe48ac479d3c3350e592192
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-03-31 18:14:14 -0600

    Added new assets and initial banner for science level

---
+++ Assets/Scripts/ScienceCanvasOperations.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScienceCanvasOperations : MonoBehaviour {
+
+    public GameObject initialBanner;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    // This method will remove the initial banner
+    public void removeBanner() {
+        initialBanner.active = false;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
