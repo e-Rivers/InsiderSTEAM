7cd3a7efe5d0838086febfbeda9cc9d7209492dd
Emilio2001Rivers2016 <rivers.emilio2001@gmail.com>
2021-04-02 23:32:36 -0600

    Added dialog box interactions in science level

--- Assets/Scripts/ScienceCanvasOperations.cs
+++ Assets/Scripts/ScienceCanvasOperations.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScienceCanvasOperations : MonoBehaviour {
 
     public GameObject initialBanner;
+    public static string sidebarAns = "", endingAns = "";
+    public Button regButton, endButton;
+    public InputField regInput, endInput;
 
     // Start is called before the first frame update
     void Start() {
@@ -19,6 +23,16 @@ public class ScienceCanvasOperations : MonoBehaviour {
     // This method will remove the initial banner
     public void removeBanner() {
         initialBanner.SetActive(false);
+    }
+
+    // Method to capture user input in every round
+    public void captureRegInput() {
+        sidebarAns = regInput.text;
+    }
+
+    // Method to capture user input at the end of the game
+    public void captureEndInput() {
+        endingAns = endInput.text;
     }
 
 }
--- Assets/Scripts/ScienceGameLogic.cs
+++ Assets/Scripts/ScienceGameLogic.cs
@@ -9,10 +9,10 @@ public class ScienceGameLogic : MonoBehaviour {
     public static int roundType = 0; //(odd is labyrinth crossing & even is answering)
     private int timeCount = 20;
     public GameObject player, mazeGenesys, initBanner, endsBanner, mazeCover;
-    public Text timeText, roundText, askText;
+    public Text timeText, roundText, askText, sciText;
     public InputField endsBannerAnswer, sidebarAnswer;
     private Dictionary<string, string> riddleDict = new Dictionary<string, string>();    
-    private bool isAskTime = false;
+    private bool isAskTime = false, labyCrossed = false;
     private Coroutine subTime;
 
     // Loads all riddles and problems
@@ -29,32 +29,28 @@ public class ScienceGameLogic : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        // Checks if the user clicked to remove the banner to start the game
-        if(!initBanner.activeSelf && roundType == 0) {
-            subTime = StartCoroutine(reduceTimer());
-            mazeGenesys.GetComponent<MazeGenerator>().GenerateMaze();
-            player.SetActive(true);
-            roundType++;
-        } else if(roundType%2 != 0) {
-            roundTypeMEM();
-        } else if(roundType != 0 && roundType%2 == 0) {
-            if(!isAskTime) { roundTypeACT(); } else { askRiddleOrProblem(); }
+        if(!labyCrossed) {
+            // Checks if the user clicked to remove the banner to start the game
+            if(!initBanner.activeSelf && roundType == 0) {
+                subTime = StartCoroutine(reduceTimer());
+                mazeGenesys.GetComponent<MazeGenerator>().GenerateMaze();
+                player.SetActive(true);
+                roundType++;
+            } else if(roundType%2 != 0) {
+                roundTypeMEM();
+            } else if(roundType != 0 && roundType%2 == 0) {
+                if(!isAskTime) { roundTypeACT(); } else { askRiddleOrProblem(); }
+            }
         }
-    
-        // Checks if the player is out of the labyrinth borders
-        if(player.transform.position.x >= 4.5 || player.transform.position.x <= -4.5 || player.transform.position.y >= 4.5 || player.transform.position.y <= -4.5) {
-            StopCoroutine(subTime);
-            mazeGenesys.GetComponent<MazeGenerator>().DeleteMaze();
-            mazeCover.SetActive(false);
-            player.SetActive(false);
-            endsBanner.SetActive(true);
-        }
+        // Checks if the player has escaped the labyrinth
+        verifyEscapeAndEnding(); 
     }
 
     // Method that implements the memorizing time
     private void roundTypeMEM() {
         if(timeCount >= 0) {
             timeText.text = "Tiempo: " + timeCount;
+            sciText.text = "Veamos que tanto puedes memorizar del laberinto...";
         } else { 
             timeCount = 20;
             roundType++;
@@ -66,30 +62,39 @@ public class ScienceGameLogic : MonoBehaviour {
         if(!isAskTime && timeCount >= 0) {
             mazeCover.SetActive(true);
             timeText.text = "Tiempo: " + timeCount;
+            sciText.text = "Este es el momento... crúzalo con lo que recuerdes...";
         } else {
             isAskTime = true;
-            timeCount = 5;
+            timeCount = 10;
             int randomSelection = Random.Range(0, riddleDict.Keys.Count);
             string randomRiddle = riddleDict.Keys.ElementAt(randomSelection);
             askText.text = randomRiddle;
+            sciText.text = "Si quieres pasar de ronda... contesta mi pregunta...";
         }
     }
     
     // Method that implements the logic to ask questions and validate the answer
     private void askRiddleOrProblem() {
         if(timeCount >= 0) {
-           timeText.text = "Tiempo: " + timeCount;
+            timeText.text = "Tiempo: " + timeCount;
+            if(ScienceCanvasOperations.sidebarAns == riddleDict[askText.text]) {
+                askText.text = "";
+                mazeCover.SetActive(false);
+                mazeGenesys.GetComponent<MazeGenerator>().DeleteMaze();
+                mazeGenesys.GetComponent<MazeGenerator>().GenerateMaze();                
+                // Calculates the current round
+                roundType++;
+                string[] prevRound = roundText.text.Split(' ');
+                roundText.text = "Ronda: " + (roundType-int.Parse(prevRound[1]));
+                timeCount = 20;            
+                isAskTime = false;           
+            } else {
+                if(ScienceCanvasOperations.sidebarAns != "") {
+                    sciText.text = "INCORRECTO! Intenta de nuevo... si es que te alcanza el tiempo...";
+                }
+            }
         } else {
-            askText.text = "";
-            mazeCover.SetActive(false);
-            mazeGenesys.GetComponent<MazeGenerator>().DeleteMaze();
-            mazeGenesys.GetComponent<MazeGenerator>().GenerateMaze();                
-            // Calculates the current round
-            roundType++;
-            string[] prevRound = roundText.text.Split(' ');
-            roundText.text = "Ronda: " + (roundType-int.Parse(prevRound[1]));
-            timeCount = 20;            
-            isAskTime = false;
+            Debug.Log("PERDISTE FRACASADO!");
         }
     }
 
@@ -100,9 +105,32 @@ public class ScienceGameLogic : MonoBehaviour {
             timeCount--; 
         }
     }
+
+    // Method to verify if the player escaped and to ask for the last question
+    private void verifyEscapeAndEnding() {
+         // Checks if the player is out of the labyrinth borders
+        if(player.transform.position.x >= 4.5 || player.transform.position.x <= -4.5 || player.transform.position.y >= 4.5 || player.transform.position.y <= -4.5) {
+            if(!labyCrossed) {
+                timeCount = 60;
+                labyCrossed = true;
+            }
+            timeText.text = "Tiempo: " + timeCount;
+            mazeGenesys.GetComponent<MazeGenerator>().DeleteMaze();
+            mazeCover.SetActive(false);
+            player.SetActive(false);
+            endsBanner.SetActive(true);
+            if(timeCount > 0) {
+                if(ScienceCanvasOperations.endingAns == "1") {
+                    StopCoroutine(subTime);
+                    Debug.Log("LO HICISTE BIEN, GANADOR!");
+                }
+            } else {
+                StopCoroutine(subTime);
+                Debug.Log("PERDISTE!");
+            }
+        }
+    }  
 }
-
-
 
 
 
