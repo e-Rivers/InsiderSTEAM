24fe79eb8fd76a7a18355f0bd0ef8481a85cb593
Youthan Irigoyen <a01750476@itesm.mx>
2021-04-28 11:41:32 -0500

    Update BackgroundSwapper.cs

--- Assets/Scripts/GENERAL/LoginScreen/BackgroundSwapper.cs
+++ Assets/Scripts/GENERAL/LoginScreen/BackgroundSwapper.cs
@@ -35,7 +35,7 @@ public class BackgroundSwapper : MonoBehaviour
             }
             bg_001.color = new Color(bg_001.color.r, bg_001.color.g, bg_001.color.b, 0f);
             bg_002.color = new Color(bg_002.color.r, bg_002.color.g, bg_002.color.b, 1f);
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(1f);
             while (bg_002.color.a > 0.05f)
             {
                 bg_002.color -= new Color(0f, 0f, 0f, 0.01f);
